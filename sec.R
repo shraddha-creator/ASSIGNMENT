f2504de9b6b937d4a5ce01fcaad1d1003745b862
shraddha-creator <66514007+shraddha-creator@users.noreply.github.com>
2020-06-06 19:28:01 +0530

    Update sec.R

--- sec.R
+++ sec.R
@@ -1,3 +1,7 @@
+#I simply set the input x as a matrix
+## and then set the solved value "s" as a null
+## then I changed every reference to "mean" to "solve"
+
 makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
   s <- NULL
   set <- function(y) {
@@ -11,7 +15,7 @@ makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
        setsolve = setsolve,
        getsolve = getsolve)
 }
-##
+## Same here, changed "mean" to "solve" and "m" to "s"
 
 cacheSolve <- function(x, ...) {
   s <- x$getsolve()
